2aebf465dd49d2ae022faeb1e070fdf22921472c
yanjunq <mandyqian58@gmail.com>
2025-07-08 21:56:16 -0700

    Fix helper function in useColumnCount

--- src/hooks/useColumnCount.ts
+++ src/hooks/useColumnCount.ts
@@ -1,16 +1,18 @@
 import { useEffect, useState } from 'react'
 
-const getColumnCount = () => {
-  if (window.matchMedia('(min-width: 1280px)').matches) return 4
-  if (window.matchMedia('(min-width: 768px)').matches) return 2
-  return 1
-}
-
 export function useColumnCount() {
-  const [count, setCount] = useState(getColumnCount())
+  const [count, setCount] = useState(1) 
 
   useEffect(() => {
+    const getColumnCount = () => {
+      if (window.matchMedia('(min-width: 1280px)').matches) return 4
+      if (window.matchMedia('(min-width: 768px)').matches) return 2
+      return 1
+    }
+
     const handler = () => setCount(getColumnCount())
+    handler()
+
     window.addEventListener('resize', handler)
     return () => window.removeEventListener('resize', handler)
   }, [])
